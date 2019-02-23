575262307f26d8914ffa23365022bca147cb3edb
Olivia Thai <olivia.d.thai@gmail.com>
2019-02-23 01:34:26 -0800

    yuhhhh

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Olivia Thai on 2/23/19.
+//  Copyright © 2019 Olivia Thai. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
